cb742589605f95ca8accc788dc02117b0eb99e40
AChemello <arthur.chemello@edu.pucrs.br>
2026-03-10 13:56:00 +0000

    Novo

--- First/src/main.c
+++ First/src/main.c
@@ -1,8 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include <stdio.h>// inclui cabeçalho de biblioteca, entrada e saida
+#include <stdlib.h>// outro, uso geral
+#include <math.h>// funções mat
+//#include <string.h>// funções de uso de string
+//C standart library headers
 
 int main() {
-    printf("Olá do projeto %s!\n", "meuprojetoaa");
+    //instruções
+    // \ significa nova linha 
+    printf("Olá do projaeto %s!\n", "First");
     return 0;
+    //int i;
+    //scanf("%d", &i)// &(obrigatório) significa 
+
+    //vetor 
+    //int A[10];
 }
